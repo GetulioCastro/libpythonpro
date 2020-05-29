0b69b63b1408e27aeefa801cd32175b94eccf6e6
Getúlio <augustoge71@gmail.com>
2020-05-28 23:04:33 -0300

    1o commit realizado pelo #devpro @GetulioCastro neste exercício, na etapa PyTools do Curso Python Pro

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -8,6 +8,10 @@ def buscar_avatar(usuario):
     :param usuario: str com o nome de usuário no github
     :return: str com o link do avatar
     """
+    # no exercício, o Renzo copia a url direto da api e muda pelo atributo da função
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('GetulioCastro'))
